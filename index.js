4ac9a4fcb4a9f073657a74587ca2aa8559ee0f5b
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-10-03 20:59:16 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "birthday";
+console.log(writeCards(names, event));
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+
+
+
+
 // Code your solutions in this file
